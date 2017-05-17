75d1f6b4681a9cb14fd798a97c759808984e6de2
22B16-PC\22B <lizbeth.borja@epn.edu.ec>
2017-05-17 12:47:49 -0500

    primera version

---
+++ boton.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal
+"""
+
+from tkinter import*
+
+root=Tk()
+boton=Button(root,text="Click me")
+boton.pack()
+root.mainloop()
